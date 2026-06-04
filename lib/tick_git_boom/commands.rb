8c5ab6aacc24f3a5c03779a51bfd8870ee1315cc
Terence Tao <terence.tao@tickgitboom.test>
2026-06-04 09:56:00 +0530

    GIT-167 Accept --help and -h as names for help

--- lib/tick_git_boom/commands.rb
+++ lib/tick_git_boom/commands.rb
@@ -15,5 +15,8 @@ module TickGitBoom
     register :Comment, 'comment', 'tick_git_boom/commands/comment'
     register :Close,   'close',   'tick_git_boom/commands/close'
     register :Help,    'help',    'tick_git_boom/commands/help'
+
+    Registry.add_alias('--help', 'help')
+    Registry.add_alias('-h', 'help')
   end
 end
